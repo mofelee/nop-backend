91fd6958ed119f499d61cd61d102bcfb9bc29158
Mofe Lee <mofe@me.com>
2015-12-19 23:07:03 +0800

    remove babel

--- src/app.js
+++ src/app.js
@@ -1,7 +1,11 @@
-import Koa from 'koa';
-import Router from 'koa-router';
+const app = require('koa')();
 
-const app = new Koa();
+app.use(async (ctx, next) => {
+  const start = new Date;
+  await next();
+  const ms = new Date - start;
+  console.log(`${ctx.method} ${ctx.url} - ${ms}ms`);
+});
 
 app.use(ctx => {
   ctx.body = 'Hello World';
